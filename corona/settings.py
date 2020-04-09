88e929a3e340e542d072dfa6537adc796b17fc32
harshG97 <harsh180397@gmail.com>
2020-04-09 21:24:11 +0900

    deployment changes

--- corona/settings.py
+++ corona/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,8 +132,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # )
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['covidvisuals.herokuapp.com', '127.0.0.1']
